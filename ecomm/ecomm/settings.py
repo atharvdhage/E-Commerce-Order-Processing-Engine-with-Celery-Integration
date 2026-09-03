a4ceefd356846b758b96d8a3f8d97ccf28842406
atharvdhage <dhageatharv06@gmail.com>
2026-09-03 12:08:24 +0530

    added postgre setup

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,12 +81,7 @@ WSGI_APPLICATION = 'ecomm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = { 'default': dj_database_url.config( default=f'sqlite:///{BASE_DIR / "db.sqlite3"}', conn_max_age=600, ) }
 
 
 # Password validation
