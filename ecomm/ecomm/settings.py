c9031e8932b7d42703e01b794bc95f267d7018dc
atharvdhage <dhageatharv06@gmail.com>
2026-09-04 16:27:07 +0530

    test

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -131,7 +131,7 @@ STORAGES = {
 # ---------------------------------------------------------
 # V3: LIVE EMAIL CONFIGURATION (Replaced dummy console mailer)
 # ---------------------------------------------------------
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
